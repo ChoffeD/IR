48774c5526018f9a4dcda965878e95a7bcd4b97f
Damien <choffe.damien@gmail.com>
2019-04-16 17:53:23 +0200

    ajout des consigne

--- Tests.php
+++ Tests.php
@@ -21,6 +21,122 @@ background-repeat: repeat;
 
 </style>
 </head>
+
+
+    <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+    <title>Unknown</title>
+    <meta name="generator" content="LibreOffice 5.3.6.1 (Linux)"/>
+    <meta name="author" content="Manu Dufait"/>
+    <meta name="created" content="2019-04-16T15:30:05"/>
+    <meta name="changedby" content="calibre"/>
+    <meta name="changed" content="2019-04-16T15:30:05"/>
+    <meta name="AppVersion" content="03.0033"/>
+    <meta name="DocSecurity" content="0"/>
+    <meta name="HyperlinksChanged" content="false"/>
+    <meta name="LinksUpToDate" content="true"/>
+    <meta name="ScaleCrop" content="false"/>
+    <meta name="ShareDoc" content="false"/>
+    <style type="text/css">
+        @page { size: 8.5in 11in; margin: 1in }
+        p { margin-bottom: 0.1in; line-height: 120% }
+    </style>
+</head>
+<body lang="en" dir="ltr">
+<p align="center" style="margin-bottom: 0in; line-height: 0.28in; page-break-before: auto; page-break-after: auto">
+    <font size="5" style="font-size: 17pt"><b>Consigne</b></font></p>
+<p style="text-indent: 0.49in; margin-bottom: 0in; line-height: 115%">
+    Dans le contexte d’un projet de recherche,&nbsp;nous avons besoin
+    de tester la ressemblance entre deux fichiers audios. Les résultats
+    de cette&nbsp;étude&nbsp;pourront faire&nbsp;avancer&nbsp;la
+    recherche dans le domaine de l’anonymisation de la voix et
+    pourront&nbsp;aboutir à&nbsp;une publication
+    scientifique.&nbsp;L’anonymisation de voix pourra être utilisée
+    pour les témoignages sensibles que vous pouvez voir à la télévision
+    par exemple.&nbsp;</p>
+<p style="margin-bottom: 0in; font-variant: normal; letter-spacing: normal; font-style: normal; line-height: 115%">
+<span style="display: inline-block; border: none; padding: 0in"><font face="Times, serif"><font size="3" style="font-size: 12pt"><font color="#000000">-D’abord,
+vous devez écouter les fichiers audios de gauche (colonne
+«&nbsp;<i>Files&nbsp;</i>»)&nbsp;;&nbsp;</span></span></font></font></font></p>
+<p style="text-indent: 0.49in; margin-bottom: 0in; line-height: 115%">
+    -Ensuite, vous devez écouter le fichier audio original associé
+    (colonne du milieu)&nbsp;;</p>
+<p style="margin-bottom: 0in; font-variant: normal; letter-spacing: normal; font-style: normal; line-height: 115%">
+<span style="display: inline-block; border: none; padding: 0in"><font face="Times, serif"><font size="3" style="font-size: 12pt"><font color="#000000">-Enfin,
+vous devez&nbsp;<u><b>attribuer une note</b></u>.&nbsp;La note doit
+être attribuée&nbsp;par rapport&nbsp;à&nbsp;la ressemblance entre
+le fichier de gauche et le fichier original. La notation se fait de&nbsp;<b>1
+à 5</b>&nbsp;(les notes décimales&nbsp;sont autorisées.&nbsp;<i>Par
+exemple&nbsp;:&nbsp;3.5</i>)&nbsp;:&nbsp;</span></span></font></font></font></p>
+<ul>
+    <li/>
+    <p style="margin-bottom: 0in; line-height: 115%"><b>1</b>&nbsp;:
+        l’enregistrement de gauche est&nbsp;<b>complètement différent</b>&nbsp;;&nbsp;</p>
+    <li/>
+    <p style="margin-bottom: 0in; line-height: 115%"><b>2</b>&nbsp;:
+        l’enregistrement de gauche est&nbsp;<b>différent</b>&nbsp;;&nbsp;&nbsp;</p>
+    <li/>
+    <p style="margin-bottom: 0in; line-height: 115%"><b>3</b>&nbsp;:&nbsp;vous
+        êtes&nbsp;<b>partagés&nbsp;</b>à la suite de l’écoute&nbsp;;&nbsp;</p>
+    <li/>
+    <p style="margin-bottom: 0in; line-height: 115%"><b>4</b>&nbsp;:&nbsp;l’enregistrement
+        de gauche est&nbsp;<b>ressemblant</b>&nbsp;;&nbsp;</p>
+    <li/>
+    <p style="margin-bottom: 0in; line-height: 115%"><b>5</b>&nbsp;:
+        l’enregistrement de gauche est&nbsp;<b>identique</b>&nbsp;à&nbsp;l'original.&nbsp;</p>
+</ul>
+<p style="margin-bottom: 0in; font-variant: normal; letter-spacing: normal; font-style: normal; line-height: 115%">
+<span style="display: inline-block; border: none; padding: 0in"><font face="Times, serif"><font size="3" style="font-size: 12pt"><font color="#000000">Vous
+verrez aussi des fichiers&nbsp;nommés «&nbsp;<i>File Transvox</i>&nbsp;»
+précédents&nbsp;chaque nouvelle série d’audio. Leur notation est
+différente. Elle est&nbsp;notée&nbsp;de 1&nbsp;à&nbsp;5&nbsp;:&nbsp;</span></span></font></font></font></p>
+<ul>
+    <li/>
+    <p style="margin-bottom: 0in; line-height: 115%"><b>1</b>&nbsp;:
+	l’intelligibilité de la personne qui parle est&nbsp;<b>mauvaise</b>&nbsp;;&nbsp;</span></p>
+    <li/>
+    <p style="margin-bottom: 0in; line-height: 115%"><b>2</b>&nbsp;:
+	l’intelligibilité de la personne qui parle est&nbsp;<b>médiocre</b>&nbsp;;&nbsp;&nbsp;</span></p>
+    <li/>
+    <p style="margin-bottom: 0in; line-height: 115%"><b>3</b>&nbsp;:
+	l’intelligibilité de la personne qui parle est&nbsp;<b>correcte</b>&nbsp;;&nbsp;&nbsp;</span></p>
+    <li/>
+    <p style="margin-bottom: 0in; line-height: 115%"><b>4</b>&nbsp;:
+	l’intelligibilité de la personne qui parle est&nbsp;<b>bonne</b>&nbsp;;&nbsp;</span></p>
+    <li/>
+    <p style="margin-bottom: 0in; line-height: 115%"><b>5</b>&nbsp;:
+	l’intelligibilité de la personne qui parle est&nbsp;<b>excellente</b>.&nbsp;</span></p>
+</ul>
+<p style="margin-bottom: 0in; font-variant: normal; letter-spacing: normal; font-style: normal; line-height: 115%">
+<span style="display: inline-block; border: none; padding: 0in"><font face="Times, serif"><font size="3" style="font-size: 12pt"><font color="#000000"><font color="#ff0000"><u><b>N’oubliez
+pas de sauvegarder en cliquant sur le bouton «&nbsp;</b></u></font><font color="#ff0000"><i><u><b>save</b></u></i></font><font color="#ff0000"><u><b>&nbsp;»</b></u></font>&nbsp;que
+vous pouvez trouver en haut de la page web ou en bas de la page
+web.&nbsp;Les consignes sont téléchargeables&nbsp;<u><b><a href="Consigne.pdf" download="Consigne">ici</a></b></u>&nbsp;pour
+les consulter si besoin au cours de la passation.&nbsp;&nbsp;</span></span></font></font></font></p>
+<p style="text-indent: 0.25in; margin-bottom: 0in; line-height: 115%">
+    <i>La durée de passation prend plusieurs heures. Vous avez la
+        possibilité de diviser le travail (<u>par exemple&nbsp;</u>: vous
+        écouter les audios jusqu’au&nbsp;«&nbsp;files&nbsp;: 4&nbsp;»&nbsp;et
+        vous sauvegardez. Les résultats nous seront envoyés. Vous pourrez
+        reprendre au fichier où vous vous étiez arrêtés).&nbsp;</i></p>
+<p style="margin-bottom: 0in; font-variant: normal; letter-spacing: normal; font-style: normal; line-height: 115%">
+<span style="display: inline-block; border: none; padding: 0in"><font face="Times, serif"><font size="3" style="font-size: 12pt"><font color="#000000">Pour
+vous remercier du temps que vous aurez pris pour ce projet, Le Loria
+(Laboratoire Lorrain de Recherche en Informatique) et l’équipe
+vous offrirons une clé USB 3.0 de 16Go.&nbsp;N’hésitez pas à
+nous contacter par
+mail&nbsp;<font color="#0563c1"><u>ilef.trabelsi@outlook.com</u></font>&nbsp;ou&nbsp;<font color="#0563c1"><u>choffe.damien@gmail.com</u></font>&nbsp;si
+vous&nbsp;souhaitez avoir un retour de votre participation et/ou pour
+recevoir la clé USB.&nbsp;&nbsp;</span></span></font></font></font></p>
+<p style="text-indent: 0.25in; margin-bottom: 0in; line-height: 115%">
+    <br/>
+
+</p>
+<p style="text-indent: 0.25in; margin-bottom: 0in; line-height: 115%">
+    <br/>
+
+</p>
+
+
 <body>
 <form action="traitement.php" method="post">
     <input type="submit" name="valide" value=" Save " />
@@ -51,8 +167,6 @@ if($ligne== 1 ){
 
     <tr>
 		<td> <h3> File Transvox </h3>  </td>
-
-
    </tr>
 
 
@@ -104,7 +218,7 @@ if($ligne== 1 ){
 
    <?php } } } $ligne = 1 ;$indice++;  } ?>
 </table>
-
+    <input type="submit" name="valide" value=" Save " />
 </form>
 </body>
 
--- traitement.php
+++ traitement.php
@@ -1,8 +1,36 @@
 <html>
 <head>
     <title>Ma page de traitement</title>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+        <title>Unknown</title>
+        <meta name="generator" content="LibreOffice 5.3.6.1 (Linux)"/>
+        <meta name="author" content="Manu Dufait"/>
+        <meta name="created" content="2019-04-16T15:30:05"/>
+        <meta name="changedby" content="calibre"/>
+        <meta name="changed" content="2019-04-16T15:30:05"/>
+        <meta name="AppVersion" content="03.0033"/>
+        <meta name="DocSecurity" content="0"/>
+        <meta name="HyperlinksChanged" content="false"/>
+        <meta name="LinksUpToDate" content="true"/>
+        <meta name="ScaleCrop" content="false"/>
+        <meta name="ShareDoc" content="false"/>
+        <style type="text/css">
+            @page { size: 8.5in 11in; margin: 1in }
+            p { margin-bottom: 0.1in; line-height: 120% }
+        </style>
 </head>
 <body>
+
+<p style="margin-bottom: 0in; font-variant: normal; letter-spacing: normal; font-style: normal; line-height: 115%">
+    <p><h3>Merci de votre participation</h3></p>
+<span style="display: inline-block; border: none; padding: 0in"><font face="Times, serif"><font size="3" style="font-size: 12pt"><span style="background: #ffffff"><font color="#000000"> N’hésitez pas à nous contacter par
+mail&nbsp;<font color="#0563c1"><u>choffe.damien@gmail.com</u></font>&nbsp;ou&nbsp;<font color="#0563c1"><u>ilef.trabelsi@outlook.com</u></font>&nbsp;si
+vous souhaitez avoir un retour de votre participation et/ou pour
+recevoir la clé USB.&nbsp;&nbsp;</span></span></font></font></font></p>
+
+
+
+
 <?php
 // on teste la déclaration de nos variables
 if (isset($_POST['valide'])) {
@@ -16,16 +44,17 @@ if (isset($_POST['valide'])) {
             preg_match('/^(File[0-9][0-9]?)$/', $key, $out);
             if(sizeof($out)>0) {
                 fputcsv($handle2, array($key, $data), ';');
-            }
-            preg_match('/File([0-9])_([0-9]_[0-9])_(-?[0-9])_wav/', $key, $keyword);
-            if (sizeof($keyword) >= 3) {
-                $file = $keyword[1];
-                $factor = $keyword[2];
-                $factor = preg_replace('/_/', '.', $factor);
-                $pitch = $keyword[3];
-                $note = $data;
-                $list[$i] = array($file, $factor, $pitch, $note);
-                $i++;
+            } else {
+                preg_match('/File([0-9])_([0-9]_[0-9])_(-?[0-9])_wav/', $key, $keyword);
+                if (sizeof($keyword) >= 3) {
+                    $file = $keyword[1];
+                    $factor = $keyword[2];
+                    $factor = preg_replace('/_/', '.', $factor);
+                    $pitch = $keyword[3];
+                    $note = $data;
+                    $list[$i] = array($file, $factor, $pitch, $note);
+                    $i++;
+                }
             }
         }
     }
@@ -34,7 +63,12 @@ if (isset($_POST['valide'])) {
     }
 
     fclose($handle);
+    if($file != 'File10')  {
+        ?> <p><h3>Vous vous etes arrete au fichier <?php echo $file?></h3></p>
+<?php
+    }
 }
 ?>
+
 </body>
 </html>
