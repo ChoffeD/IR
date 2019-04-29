12372c18d04b4107ba167813e5d5597b2726da76
Damien <choffe.damien@gmail.com>
2019-04-29 10:26:39 +0200

    maj programme de merge csv

--- src_merge/Merge.java
+++ src_merge/Merge.java
@@ -1,6 +1,9 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -8,6 +11,7 @@ import java.util.Map;
 public class Merge {
 
 	public static final String sep = ";";
+	private static DecimalFormat  df;
 
 	public static final Map<String, Integer> Factor2Int = new HashMap<>() {{
 		put("0.5", 0);
@@ -27,20 +31,20 @@ public class Merge {
 	}};
 
 	public static final Map<Integer, String> Int2Factor = new HashMap<>() {{
-		put(0,"0.5");
-		put(1,"0.6");
-		put(2, "0.7");
-		put(3, "0.8");
-		put(4, "0.9");
-		put(5, "1.0");
-		put(6, "1.1");
-		put(7, "1.2");
-		put(8, "1.3");
-		put(9, "1.4");
-		put(10, "1.5");
-		put(11, "1.6");
-		put(12, "1.7");
-		put(13, "1.8");
+		put(0,"0,5");
+		put(1,"0,6");
+		put(2, "0,7");
+		put(3, "0,8");
+		put(4, "0,9");
+		put(5, "1,0");
+		put(6, "1,1");
+		put(7, "1,2");
+		put(8, "1,3");
+		put(9, "1,4");
+		put(10, "1,5");
+		put(11, "1,6");
+		put(12, "1,7");
+		put(13, "1,8");
 	}};
 
 	public static final Map<Integer, String> Int2Pitch = new HashMap<>() {{
@@ -64,6 +68,11 @@ public class Merge {
 	}};
 
 	public static void merge_csv(String directoryPath) {
+		df = new DecimalFormat("#####0.00");
+		DecimalFormatSymbols dfs = df.getDecimalFormatSymbols();
+		dfs.setDecimalSeparator(',');
+		df.setDecimalFormatSymbols(dfs);
+		
 		File directory = new File(directoryPath);       
 		File[] files = directory.listFiles();
 		float[][][] contenue = new float[10][14][7];
@@ -84,6 +93,7 @@ public class Merge {
 		}
 		contenueFinal = addMoyenne(contenueFinal);
 		writeCsv(contenueFinal, directoryPath+"\\data_final.csv");
+		writeCsvMeanV2(contenueFinal, directoryPath+"\\data_for_graphe.csv", directoryPath+"\\data_for_grapheH.csv", directoryPath+"\\data_for_grapheF.csv");
 
 	}
 
@@ -117,9 +127,95 @@ public class Merge {
 		return dataModifie;
 	}
 
+
+	public static void writeCsvMeanV2(float[][][][] data, String filename, String filenameH, String filenameF) {
+		try {
+			File test1 = new File(filename);
+			PrintWriter writer = new PrintWriter(test1);
+
+			File testH = new File(filenameH);
+			PrintWriter writerH = new PrintWriter(testH);
+
+			File testF = new File(filenameF);
+			PrintWriter writerF = new PrintWriter(testF);
+
+
+
+			String ligne;
+			int length = data[0][0][0].length;
+
+			data = addMoyenne(data);
+			ligne = ""+sep+"-3"+sep+"-2"+sep+"-1"+sep+"0"+sep+"1"+sep+"2"+sep+"3";
+			writer.print(ligne);
+			writer.print("\n");
+			writerH.print(ligne);
+			writerH.print("\n");
+			writerF.print(ligne);
+			writerF.print("\n");
+
+			String ligneH, ligneF;
+
+			int tot1, nb1, nb2, tot2;
+			float moy;
+			for (int j = 0; j < data[0].length; j++) {
+				ligne =Int2Factor.get(j);
+				ligneH =Int2Factor.get(j);
+				ligneF =Int2Factor.get(j);
+				for (int j2 = 0; j2 < data[0][j].length; j2++) {
+					tot1 = 0;
+					tot2 = 0;
+					nb1 = 0;
+					nb2 = 0;
+					for(int i = 0; i < 5; i++) {
+						if(data[i][j][j2][length] != 0) {
+							nb1++;
+							tot1 += data[i][j][j2][length];
+						}
+					}
+					if(nb1 != 0) {
+						moy = tot1/(float)nb1;
+						ligneH += sep+df.format(moy);
+					} else {
+						ligneH +=sep;
+					}
+
+					for(int i = 5; i < 10; i++) {
+						if(data[i][j][j2][length] != 0) {
+							nb2++;
+							tot2 += data[i][j][j2][length];
+						}
+					}
+					if(nb1 != 0) {
+						moy = tot2/(float)nb2;
+						ligneF += sep+df.format(moy);
+					} else {
+						ligneF +=sep;
+					}
+					if(nb1 + nb2 != 0) {
+						moy = (tot1+tot2)/(float)(nb1+nb2);
+						ligne += sep+df.format(moy);
+					} else {
+						ligne +=sep;
+					}
+				}
+				writerH.print(ligneH+"\n");
+				writerF.print(ligneF+"\n");
+				writer.print(ligne+"\n");
+			}
+
+
+			writer.close();
+			writerH.close();
+			writerF.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	
 	public static void writeCsv(float[][][][] data, String filename) {
 		try{
-			System.out.println(filename);
 			File test1 = new File(filename);
 			PrintWriter writer = new PrintWriter(test1);
 			String ligne;
@@ -127,7 +223,7 @@ public class Merge {
 			for (int i = 0; i < data[0][0][0].length-1; i++) {
 				ligne+=sep+"Testeur"+i;
 			}
-			ligne+=sep+"Moyenne";
+			ligne+=sep+"Moyenne"+sep+"Sexe";
 			writer.print(ligne);
 			writer.print("\n");
 			for (int i = 0; i < data.length; i++) {
@@ -137,10 +233,14 @@ public class Merge {
 						ligne =(i+1)+sep+Int2Factor.get(j)+sep+Int2Pitch.get(j2);
 						for (int k = 0; k < data[i][j][j2].length; k++) {
 							if(data[i][j][j2][k] != 0)
-								ligne+= sep+data[i][j][j2][k];
+								ligne+= sep+df.format(data[i][j][j2][k]);
 							else 
 								ligne+=sep;
 						}
+						if(i <= 4) {
+							ligne += sep+"Homme";
+						} else
+							ligne += sep+"Femme";
 						writer.print(ligne);
 						writer.print("\n");
 					}
